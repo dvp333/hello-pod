c37dd87074a39c6d3ef6584afa475f06dac0ca47
Diego Prado <dvp333@gmail.com>
2022-01-14 14:14:00 -0300

    test

---
+++ HelloPod/HelloPod.swift
@@ -0,0 +1,17 @@
+//
+//  HelloPod.swift
+//  HelloPod
+//
+//  Created by Diego Prado on 14/01/22.
+//
+
+import Foundation
+import UIKit
+// import SocureSdk
+
+public class HelloPod {
+ 
+    public func getVersion() -> String {
+    return "Yeah!!! iOS " + UIDevice.current.systemVersion
+  }
+}
