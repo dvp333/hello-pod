3f95a7029a188790409fe28b4220b3ea307857d9
Diego Prado <dvp333@gmail.com>
2022-01-14 14:05:22 -0300

    Initial Commit

---
+++ HelloPodTests/HelloPodTests.swift
@@ -0,0 +1,33 @@
+//
+//  HelloPodTests.swift
+//  HelloPodTests
+//
+//  Created by Diego Prado on 14/01/22.
+//
+
+import XCTest
+@testable import HelloPod
+
+class HelloPodTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
